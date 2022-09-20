8c1602229a0ae08f7c9fd85fcf553e8c9c7cb77d
AjithKumar-007 <ajithkumar2626@gmail.com>
2022-09-20 10:26:32 +0530

    Day 1 Modified

--- ArithmeticOperators.java
+++ ArithmeticOperators.java
@@ -12,20 +12,11 @@ public class ArithmeticOperators {
 		int sub=sum3-sum4;
 		System.out.println(sub);
 		
-		int sum5=15;
-		int sum6=10;
-		int multi=sum5*sum6;
+		int n1=15;
+		int n2=10;
+		int multi=n1*n2;
 		System.out.println(multi);
 		
-		int sum7=20;
-		int sum8=10;
-		int div=sum7/sum8;
-		System.out.println(div);
-		
-		int sum9=27;
-		int sum10=5;
-		int mod=sum9%sum10;
-		System.out.println(mod);
 		
 		
 		
